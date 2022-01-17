b745e6f30ba8c3df3b7871fef2a72a5fcc88fbd1
JesusWasTaken <heard.philippe@gmail.com>
2022-01-17 08:37:52 +0100

    Update targeted.js

--- modules.js/bulletTypes/targeted.js
+++ modules.js/bulletTypes/targeted.js
@@ -4,27 +4,31 @@ export default class Targeted extends Bullets {
     constructor(posX, posY, enemy, target) {
         super(posX, posY, enemy);
         this.target = target;
-        this.getVector();
-        console.log(this.vector);
+        this.getAngle();
         
     }
 
     move() {
-        let d = Math.floor(Math.sqrt(Math.pow(this.speed,2)/Math.pow(this.vector+1,2)))
-        if (d < this.speed/3) {
-            d = this.speed/3;
-        } else if (d > this.speed*2) {
-            d = this.speed * 2;
-        }
+        // let d = Math.floor(Math.sqrt(Math.pow(this.speed,2)/Math.pow(this.vector+1,2)))
+        // if (d < this.speed/3) {
+        //     d = this.speed/3;
+        // } else if (d > this.speed*2) {
+        //     d = this.speed * 2;
+        // }
 
-        this.posX += this.vector*d;
+        // this.posX += this.vector*d;
         
-        this.posY += d;
+        // this.posY += d;
+        vX = this.speed*Math.cos(angle);
+        vY = this.speed*Math.sin(angle);
+
+        this.posX += vX;
+        this.posY += vY;
     }
 
-    getVector() {
+    getAngle() {
         
-        this.vector = (this.target.posX-this.posX) / (this.target.posY-this.posY);
+        this.angle = Math.atan2(this.target.posY - this.posY, this.target.posX - this.posX) * 180 / Math.PI;
         
     }
 }
